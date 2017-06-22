da89f6d3494851d776fce29f34b8d4ce6e385ee5
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2017-06-23 00:42:02 +0200

    [:white_check_mark: TESTS] Add DOM Injector and Logger tests
    Fixes ---
    Related to ---

--- test/test.js
+++ test/test.js
@@ -21,10 +21,9 @@ const read = require('read-file');
 var html = read.sync('./index.html', 'utf8');
 
 
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
-
-var dom = new JSDOM(html);
+const dom = new JSDOM(html);
 
 global.window = dom.window;
 global.document = dom.window.document;
@@ -39,6 +38,27 @@ describe('DeviceInfo', () => {
     describe('#DeviceInfo()', () => {
         it('should return an object', () => {
             assert.ok(typeof DeviceInfo() === 'object');
+        });
+    });
+
+    describe('#DeviceInfo().log()', () => {
+        it('should succeed for valid console function', () => {
+            assert.ok(DeviceInfo().log('debug', 'test'));
+        });
+        it('should fail for invalid console function', () => {
+            assert.ok(DeviceInfo().log('something', 'test') === false);
+        });
+        it('should fail for missing param', () => {
+            assert.ok(DeviceInfo().log('debug') === false);
+        });
+    });
+
+    describe('#DeviceInfo().injectIntoDom()', () => {
+        it('should succeed on existing DOM elements', () => {
+            assert.ok(DeviceInfo().injectIntoDom('window-size', 'Maximized'));
+        });
+        it('should succeed on existing DOM elements', () => {
+            assert.ok(DeviceInfo().injectIntoDom('lorem-ipsum', 'test') === false);
         });
     });
 
@@ -80,7 +100,7 @@ describe('DeviceInfo', () => {
             });
             it('should set content to "1024x768"', () => {
                 DeviceInfo().win.setWindowInnerDimension();
-                const content = document.getElementById('window-inner-dimension').lastElementChild.innerHTML;
+                const content = document.getElementById('window-inner-dimension').lastElementChild.textContent;
                 assert.ok(content === '1024x768');
             });
         });
@@ -112,7 +132,7 @@ describe('DeviceInfo', () => {
             });
             it('should set content to "1024x768"', () => {
                 DeviceInfo().win.setWindowOuterDimension();
-                const content = document.getElementById('window-outer-dimension').lastElementChild.innerHTML;
+                const content = document.getElementById('window-outer-dimension').lastElementChild.textContent;
                 assert.ok(content === '1024x768');
             });
         });
@@ -155,7 +175,7 @@ describe('DeviceInfo', () => {
             it('should set content to "1024x768"', () => {
                 if (window.screen.availWidth !== undefined && window.screen.availHeight !== undefined) {
                     DeviceInfo().win.setWindowMaxDimension();
-                    const content = document.getElementById('window-max-dimension').lastElementChild.innerHTML;
+                    const content = document.getElementById('window-max-dimension').lastElementChild.textContent;
                     assert.ok(content === '1024x768');
                 }
             });
