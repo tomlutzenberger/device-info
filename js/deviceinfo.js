a830b35d051780789e106279c9b05e75e5ebd772
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2017-06-22 03:39:17 +0200

    [:lipstick: STYLING] Add basic layout
    Fixes #2
    Related to ---

---
+++ js/deviceinfo.js
@@ -0,0 +1,21 @@
+/**
+ * DeviceInfo v0.1
+ * Dynamic Bookmarklet to quickly access the backend of your tool
+ *
+ * Copyright (c) 2017 - Tom Lutzenberger (lutzenbergerthomas at gmail dot com)
+ * https://github.com/tomlutzenberger/device-info
+ *
+ * @license: Licensed under the MIT license
+ * https://github.com/tomlutzenberger/device-info/blob/master/LICENSE
+ * https://tomlutzenberger.github.io/device-info/
+ */
+
+/*globals document,window*/
+/*jslint esnext:true */
+
+
+const DeviceInfo = () => {
+
+
+
+};
