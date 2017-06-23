c3939263f73d7416c2a3498ccb85207deb4a6723
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2017-06-23 22:46:31 +0200

    [:white_check_mark: TESTS] Add tests for screen information
    Fixes #8
    Related to #4

--- test/test.js
+++ test/test.js
@@ -23,7 +23,8 @@ var html = read.sync('./index.html', 'utf8');
 
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
-const dom = new JSDOM(html);
+const options = {};
+const dom = new JSDOM(html, options);
 
 global.window = dom.window;
 global.document = dom.window.document;
@@ -57,6 +58,23 @@ describe('DeviceInfo', () => {
         });
         it('should fail for missing param', () => {
             assert.ok(DeviceInfo().log('debug') === false);
+        });
+    });
+
+    describe('#DeviceInfo().typeCheck()', () => {
+        it('should succeed on integer', () => {
+            const testValue = 123;
+            assert.equal(DeviceInfo().typeCheck(testValue, 'test'), testValue);
+        });
+        it('should succeed on strings', () => {
+            const testValue = 'lorem-ipsum';
+            assert.equal(DeviceInfo().typeCheck(testValue, 'test'), testValue);
+        });
+        it('should fail on undefined', () => {
+            assert.ok(DeviceInfo().typeCheck(undefined, 'test') === 'undefined');
+        });
+        it('should fail on null', () => {
+            assert.ok(DeviceInfo().typeCheck(null, 'test') === 'undefined');
         });
     });
 
@@ -193,6 +211,151 @@ describe('DeviceInfo', () => {
                 }
             });
         });
+    });
+
+
+
+    /**
+     * @description Screen functions
+     *
+     */
+    describe('DeviceInfo.screen', () => {
+
+        describe('#screen', () => {
+            it('should return an object', () => {
+                assert.ok(typeof DeviceInfo().screen === 'object');
+            });
+            it('should not be an empty object', () => {
+                assert.ok(Object.keys(DeviceInfo().screen).length > ZERO);
+            });
+        });
+
+
+        describe('#getWidth()', () => {
+            it('should return an integer', () => {
+                assert.ok(Number.isInteger(DeviceInfo().screen.getWidth()));
+            });
+        });
+
+
+        describe('#getHeight()', () => {
+            it('should return an integer', () => {
+                assert.ok(Number.isInteger(DeviceInfo().screen.getHeight()));
+            });
+        });
+
+
+        describe('#setDimension()', () => {
+            it('should return true', () => {
+                assert.ok(DeviceInfo().screen.setDimension());
+            });
+            it('should set content to "0x0"', () => {
+                // Because jsDOM's window is 0x0 pixel
+                DeviceInfo().screen.setDimension();
+                const content = document.getElementById('screen-dimension').lastElementChild.textContent;
+                assert.ok(content === '0x0');
+            });
+        });
+
+
+        describe('#getPixelRatio()', () => {
+            it('should return an integer', () => {
+                if (window.devicePixelRatio !== undefined) {
+                    assert.ok(Number.isInteger(DeviceInfo().screen.getPixelRatio()));
+                }
+            });
+            it('should be greater than zero', () => {
+                if (window.devicePixelRatio !== undefined) {
+                    assert.ok(DeviceInfo().screen.getPixelRatio() > ZERO);
+                }
+            });
+        });
+
+
+        describe('#setPixelRatio()', () => {
+            it('should return true', () => {
+                if (window.devicePixelRatio !== undefined) {
+                    assert.ok(DeviceInfo().screen.setPixelRatio());
+                }
+            });
+            it('should set content to "1x"', () => {
+                if (window.devicePixelRatio !== undefined) {
+                    DeviceInfo().screen.setPixelRatio();
+                    const content = document.getElementById('screen-pixel-ratio').lastElementChild.textContent;
+                    assert.ok(content === '1x');
+                }
+            });
+        });
+
+
+        describe('#getColorDepth()', () => {
+            it('should return an integer', () => {
+                if (window.screen.colorDepth !== undefined) {
+                    assert.ok(Number.isInteger(DeviceInfo().screen.getColorDepth()));
+                }
+            });
+            it('should be greater than zero', () => {
+                if (window.screen.colorDepth !== undefined) {
+                    assert.ok(DeviceInfo().screen.getColorDepth() > ZERO);
+                }
+            });
+        });
+
+
+        describe('#setColorDepth()', () => {
+            it('should return true', () => {
+                if (window.screen.colorDepth !== undefined) {
+                    assert.ok(DeviceInfo().screen.setColorDepth());
+                }
+            });
+            it('should set content to "24Bit"', () => {
+                if (window.screen.colorDepth !== undefined) {
+                    DeviceInfo().screen.setColorDepth();
+                    const content = document.getElementById('screen-color-depth').lastElementChild.textContent;
+                    assert.ok(content === '24Bit');
+                }
+            });
+        });
+
+
+        describe('#getScreenType()', () => {
+            it('should return a string', () => {
+                assert.ok(typeof DeviceInfo().screen.getScreenType() === 'string');
+            });
+        });
+
+
+        describe('#setScreenType()', () => {
+            it('should return true', () => {
+                assert.ok(DeviceInfo().screen.setScreenType());
+            });
+            it('should set content to "Primary Screen"', () => {
+                DeviceInfo().screen.setScreenType();
+                const content = document.getElementById('screen-type').lastElementChild.textContent;
+                assert.ok(content === 'Primary Screen');
+            });
+        });
+
+
+        describe('#getScreenPosition()', () => {
+            it('should return a string', () => {
+                assert.ok(typeof DeviceInfo().screen.getScreenPosition() === 'string');
+            });
+        });
+
+
+        describe('#setScreenPosition()', () => {
+            it('should return true', () => {
+                assert.ok(DeviceInfo().screen.setScreenPosition());
+            });
+            it('should set content to "Center"', () => {
+                DeviceInfo().screen.setScreenPosition();
+                const content = document.getElementById('screen-position').lastElementChild.textContent;
+                assert.ok(content === 'Center');
+            });
+        });
+
+
     });
 
 });
